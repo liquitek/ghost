85ba769ecb34f015061f9e876ed55c4bb911b7ff
Seva <sevakode@gmail.com>
2024-04-04 07:46:43 +0700

    Add localization support with i18next and language switcher
    - Integrate i18next library for localization
    - Define translations for English and Russian languages
    - Add data-i18n attributes to HTML elements for text localization
    - Implement updateTexts() function to update localized texts
    - Create language switcher with active state and hover effects
    - Style the language switcher for desktop and mobile views
    - Move slider initialization to main.js

---
+++ main.js
@@ -0,0 +1,322 @@
+i18next
+.use(i18nextBrowserLanguageDetector)
+.init({
+debug: true,
+fallbackLng: 'en',
+resources: {
+
+    "en": {
+        "translation": {
+          "nav": {
+            "reviews": "Reviews",
+            "cases": "Cases",
+            "faq": "FAQ",
+            "team": "Team",
+            "submit": "Submit Application"
+          },
+          "hero": {
+            "title": "Integrating AI into Your Company's Business Processes",
+            "subtitle": "Increase profits and reduce costs by implementing AI solutions that automate and optimize your employees' work",
+            "button": "Discuss Task"
+          },
+          "problems": {
+            "title": "Integrating AI into your company's business processes",
+            
+            "0": {
+              "heading": "Procedures for hiring new employees",
+              "text": "The state is constantly growing. You face the challenge of <br> finding quality specialists among the diversity of the labor market. You have to turn to <br> recruiters or individual agencies, <em>spending significant resources on this.</em> ",
+
+            },
+            "1": "Employee adaptations",
+            "2": "Human factor",
+            "3": "High operational costs",
+            "4": "Huge amounts of data",
+            "5": "Spending on linear tasks"
+          },
+          "form": {
+            "title": "We'll help you!",
+            "subtitle": "Just submit your application and our manager will contact you...",
+            "contact": "Contact for communication",
+            "revenue": "Approximate company turnover per year",
+            "activity": "Activity, website",
+            "submit": "Send"
+          },
+          "quotes": {
+            "0": {
+              "name": "Violetta Vasilenko",
+              "position": "Tutor",
+              "quote": "Implementing a neural employee into the educational process has been a great breakthrough for us!"
+            },
+            "1": {
+              "name": "Natalya Mizudo",
+              "position": "Psychologist",
+              "quote": "Many thanks to AI Solutions for integrating a neural psychologist-employee into my psychological practice!"
+            },
+            "2": {
+              "name": "Typhoon Roasters",
+              "quote": "The GOST AI Solutions team has done incredibly much for our company."
+            }
+          },
+          "cases": "Our neuro-assistant will tell you about our cases!",
+          "faq": {
+            "0": "Can you integrate AI into our complex operational activities?",
+            "1": "Can AI adequately perceive all feedback from customers?",
+            "2": "Is your solution really going to reduce my costs?"
+          },
+          "partners": "Our Partners",
+          "integration_steps": {
+            "0": "Submit your application, we will contact you within one business day.",
+            "1": "At the first meeting, the manager will conduct a brief diagnosis of your business.",
+            "2": "The second meeting will be with a business analyst.",
+            "3": "After approving the estimate, we will draw up a contract.",
+            "4": "After payment, we will start work.",
+            "5": "After integration, we will demonstrate the final product to you.",
+            "6": "We'll be friends for many more years, implementing AI into new processes."
+          },
+          "team": {
+            "0": {
+              "name": "Sevak Petrosyan",
+              "position": "Technical Director",
+              "description": "10 years in IT, engineer, analyst"
+            },
+            "1": {
+              "name": "Saikovsky Ivan",
+              "position": "CEO",
+              "description": "7 years in marketing, entrepreneur, marketer"
+            },
+            "2": {
+              "name": "Anna Khan",
+              "position": "Sales Director",
+              "description": "10 years in sales, business technologist, crisis manager"
+            }
+          },
+          "contacts": {
+            "inn": "246525853385",
+            "ogrnip": "320246800036639",
+            "residency": "Agency for Innovative Development",
+            "phone": "+7 (913) 532-31-67",
+            "email": "info@ascender.space",
+            "privacy_policy": "Privacy Policy",
+            "terms_of_service": "Terms of Service",
+            "address": "St. Petersburg, Kazanskaya Street, 7B"
+          }
+        }
+      },
+
+      
+      "ru": {
+        "translation": {
+          "nav": {
+            "reviews": "Отзывы",
+            "cases": "Кейсы",
+            "faq": "FAQ",
+            "team": "Команда",
+            "submit": "Обсудить задачу"
+          },
+          "hero": {
+            "title": "Интеграция ИИ в бизнес-процессы вашей компании",
+            "subtitle": "Увеличим прибыль и сократим расходы с помощью внедрения ИИ-решений, автоматизирующих и оптимизирующих работу ваших сотрудников",
+            "button": "Обсудить задачу"
+          },
+          "problems": {
+            "title": "Если вы регулярно теряете прибыль из-за...",
+            "0": {
+              "title": "Процедуры найма новых сотрудников",
+              "text": "Штат постоянно увеличивается. Сталкиваетесь со сложностью поиска качественных специалистов среди многообразия рынка труда. Приходится обращаться к рекрутерам или отдельным агентствам, <em>тратя на это немалые средства.</em>"
+            },
+            "1": {
+              "title": "Адаптации сотрудников",
+              "text": "У вас сложный продукт, нет чётких регламентов и обучающих программ. Каждый сотрудник долго вводится в должность и не сразу начинает выполнять поставленные задачи, что <em>ведёт к издержкам на оплату труда.</em>"
+            },
+            "2": {
+              "title": "Человеческого фактора",
+              "text": "Сотрудники работают менее эффективно, чем предполагает бизнес-модель (болеют, совершают ошибки и т.д.), не справляются с большими нагрузками (общение с несколькими клиентами/учениками одновременно). <em>Издержки на фонд оплаты труда в разы выше, чем прибыль, которую приносит команда проекта.</em>"
+            },
+            "3": {
+              "title": "Высоких операционных затрат",
+              "text": "Квалифицированные сотрудники, участвующие в сложных бизнес-процессах требуют большой оплаты труда, вследствие чего <em>падает общая маржинальность проекта.</em>"
+            },
+            "4": {
+              "title": "Огромного количества данных",
+              "text": "Вы или ваши сотрудники не можете принять верное стратегическое решение, поскольку на прогнозирование результата влияет огромное количество бизнес-показателей. <em>Как следствие, теряете прибыль из-за ошибок в планировании.</em>"
+            },
+            "5": {
+              "title": "Трат на линейные задачи",
+              "text": "В вашем бизнесе <em>много простых процессов</em> и их можно эффективно заменить роботизированным сотрудником, который, в отличие от человека, не требует больших ресурсов на содержание.</em>"
+            }
+          },
+          "form": {
+            "title": "Мы вам поможем!",
+            "subtitle": "Просто оставьте заявку и с вами свяжется наш менеджер: проконсультирует по всем вопросам, сделает расчёт затрат на внедрение нашего решения и прогноз прибыли.",
+            "contact": "Контакт для связи",
+            "revenue": "Примерный оборот компании в год",
+            "activity": "Деятельность, сайт",
+            "submit": "Отправить"
+          },
+          "quotes": {
+            "title": "<em>Ваши слова</em> — наша <em>основная</em> мотивация",
+            "0": {
+              "name": "Виолетта Василенко",
+              "position": "Тьютор, <a href=\"https://t.me/tutor_viola\" class=\"site\" target=\"_blank\">t.me/tutor_viola</a>",
+              "quote": "Внедрение нейросотрудника в образовательный процесс стало для нас очень крутым прорывом!<br><br>Если раньше мы тратили по 25-30 минут на один комментарий-отчёт к занятию, то сейчас за это время можно написать 5 полноценных комментариев! Вдумайтесь только: это в 5 (!) раз больше! В педагогике нельзя писать просто по шаблону, ведь каждый тьюторант индивидуален! Теперь обратная связь по каждому из них стала более полноценной и корректной.<br>Время, затрачиваемое тьютором на анализ тьюториала, уменьшилось в несколько раз. Этот опыт действительно превзошёл наши ожидания и улучшил качество отчётов к занятиям.<br><br>Спасибо GOST AI Solutions!"
+            },
+            "1": {
+              "name": "Наталья Мизудо",
+              "position": "Психолог",
+              "quote": "Большое спасибо AI Solutions за интеграцию нейросотрудника-психолога в мою психологическую практику! Это решение сделало мою работу в разы эффективнее. Теперь я предоставляю своим клиентам более глубокие разборы за очень короткий срок и могу выделить больше внимания каждому клиенту и лично себе. Мои клиенты уже отметили значительное улучшение качества услуг! Ещё раз спасибо за возможность использовать такую инновационную технологию в своей работе!"
+            },
+            "2": {
+              "name": "<a href=\"https://typhoon.coffee/\" target=\"_blank\">Typhoon Roasters</a>",
+              "quote": "Команда GOST AI Solutions сделала невероятно много для нашей компании. Внедрение их инновационных решений на базе искусственного интеллекта стало ключевым шагом в решении наших проблем. В частности, внедрение нейросотрудника привело к значительному улучшению наших процессов обучения новых сотрудников и управления данными.<br>Мы глубоко признательны за профессионализм и энтузиазм, с которыми команда GOST AI Solutions подошла к работе. Сотрудничество с ними было не только приятным, но и крайне продуктивным.<br><br>Благодарим за ваше ценное участие в развитии Typhoon Roasters."
+            }
+          },
+          "cases": {
+            "title": "Наш <em>нейро-ассистент</em> расскажет вам о наших <em>кейсах!</em>",
+            "description": "Мы открыто описываем все наши кейсы. Вы можете спросить у нейро-ассистента всё, что хотите узнать о любом из представленных проектов."
+          },
+          "faq": {
+            "title": "Быстрые <em>ответы</em> на <em>частые</em> вопросы",   
+            "0": {
+              "question": "Сможете ли вы встроить ИИ в нашу сложную операционную деятельность?",
+              "answer": "Да, конечно сможем. Более того, мы её упростим. Прежде чем встраивать ИИ в вашу деятельность, мы проведём подробный аудит и предложим решения по оптимизации ваших бизнес-процессов, только после этого будем приступать к интеграции."
+            },
+            "1": {
+              "question": "Сможет ли ИИ адекватно воспринимать всю обратную связь от клиентов? В том числе сложные запросы?",
+              "answer": "Да, конечно сможет. ИИ воспринимает информацию на уровне человека со степенью магистра, более того, интеграция ИИ в ваш бизнес подразумевает, что он также будет учитывать всю внутреннюю информацию вашей компании: скрипты, регламенты, тон общения с клиентом и так далее."
+            },
+            "2": {
+              "question": "Точно ли ваше решение уменьшит мои издержки и я не солью деньги «в никуда» в очередной раз?",
+              "answer": "Прежде чем начинать работу, мы детально просчитаем экономику конкретно для вашей компании. Наше решение точно будет эффективным, а численные показатели мы сможем озвучить после оценки проекта."
+            },
+            "3": {
+              "question": "Сможет ли ИИ полностью взаимодействовать с клиентом или ему нужна будет поддержка сотрудника?",
+              "answer": "Это зависит от задач, которые будет решать ИИ в вашем бизнесе, и его функционала. Например, во внутреннем обучении сотрудников и их консультировании по базе знаний компании ИИ не нуждается в помощи, а в работе с отзывами будет нуждаться в контроле человека (но при этом увеличит производительность сотрудника за ту же единицу времени в 4 раза)."
+            },
+            "4": {
+              "question": "Безопасно ли внедрение ИИ? Не «сломает» ли он мне весь бизнес? Смогу ли я его контролировать?",
+              "answer": "Да, внедрение ИИ совершенно безопасно, так как происходит поэтапно и в конкретно выбранном бизнес-процессе. Также важно отметить, что прежде, чем начать работу «в полях», интеграция проходит несколько этапов тестирований, во время которых все ошибки устраняются и вносятся корректировки для более качественной работы."
+            },
+            "5": {
+              "question": "Гарантируете ли вы безопасность моих данных?",
+              "answer": "Да, гарантируем абсолютную безопасность ваших данных, так как мы используем проверенные серверы по вашим требованиям, на которых установлены локальные модели."
+            }
+          },
+          "partners": "Наши <em>партнёры</em>",
+          "integration_steps": {
+            "title": "Продукт <em>сложный</em>, но его внедрение будет для вас <em>простым</em>!",
+            "0": "Оставьте заявку, мы свяжемся с вами в течение рабочего дня. Провести консультацию сможем на online-созвоне или на личной встрече.",
+            "1": "На первой встрече менеджер проведет краткую диагностику вашего бизнеса, которая поможет понять, чем мы можем быть для вас полезны. Также подробнее расскажет про услуги интеграции и ответит на ваши вопросы.",
+            "2": "Вторая встреча пройдет с бизнес-аналитиком. Он детально разберет процессы вашего бизнеса, составит план работ, ресурсных затрат, ЦКП и смету для утверждения.",
+            "3": "После утверждения сметы составим договор, согласуем его и подпишем.",
+            "4": "После оплаты начнем работу. Запросим у вас всю необходимую информацию и доступы.", 
+            "5": "Закончив интеграцию, продемонстрируем вам финальный продукт, обучим работе с ним, будем поддерживать и дорабатывать продукт до полной автономной работы.",
+            "6": "Дружим еще долгие годы, внедряя ИИ в новые процессы, потому что их потенциал безграничен :)"
+          },
+          "team": {
+            "title": "Мы собрали команду <em>экспертов</em>, которую хотим с гордостью представить",
+            "0": {
+              "name": "Севак Петросян",
+              "position": "Технический директор",
+              "description": "<span>10 лет в IT, инженер, аналитик</span><div class=\"mt-2\">Работал с PSPCatalog, YogaDog, Traffic Devils<div></div></div>"
+            },
+            "1": {
+              "name": "Сайковский Иван",
+              "position": "Генеральный директор", 
+              "description": "<span>7 лет в маркетинге, Предприниматель, маркетолог</span><div class=\"mt-2\">Работал с РЖД, Тинькофф, ArtMasters, ATLAS Agency, Точка ИМПРО<div></div></div>"
+            },
+            "2": {
+              "name": "Анна Кан",
+              "position": "Директор по продажам",
+              "description": "<span>10 лет в продажах, бизнес-технолог, кризис-менеджер</span><div class=\"mt-2\">Работала с Club500, Макбургерс, Универсиада-2019, GlushkoGroup</div>"
+            }
+          },
+          "contacts": {
+            "title": "<em>ИНФОРМАЦИЯ</em>",
+            "inn": "246525853385",
+            "ogrnip": "320246800036639", 
+            "residency": "«Агенства инновационного развития»",
+            "phone": "+7 (913) 532-31-67",
+            "email": "info@ascender.space",
+            "privacy_policy": "Политика конфинденциальности",
+            "terms_of_service": "Условия оферты",
+              "address": "г. Санкт-Петербург, ул. Казанская, д. 7Б"
+            }
+          }
+        }
+            
+
+}
+});
+function updateTexts() {
+    const elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach(el => {
+      const keys = el.getAttribute('data-i18n').split(';'); // Если несколько ключей, разделенных ;
+      let text = '';
+      keys.forEach(key => {
+        text += i18next.t(key.trim()) + ' '; // Получаем перевод для каждого ключа и добавляем к тексту
+      });
+      el.innerHTML = text.trim(); // Устанавливаем HTML-содержимое элемента
+    });
+  }
+  
+document.addEventListener('DOMContentLoaded', function() {
+// Select all elements with the 'language-link' class
+var languageLinks = document.querySelectorAll('.lang-link');
+
+languageLinks.forEach(function(link) {
+   // Attach a click event listener to each link
+   link.addEventListener('click', function(e) {
+       // Prevent the default link action
+       e.preventDefault();
+
+       // Change the language to the one specified in the 'data-lang' attribute
+       var lang = this.getAttribute('data-lang');
+       i18next.changeLanguage(lang, () => {
+        updateTexts(); // Обновляем тексты после смены языка
+       });
+
+
+       // Remove the 'active' class from all links
+       languageLinks.forEach(function(lnk) {
+           lnk.classList.remove('active');
+       });
+
+       // Add the 'active' class to the clicked link
+       this.classList.add('active');
+   });
+});
+});
+
+
+ function sliders() {
+    let viewport;
+    var p = new Splide('.problem-slider').mount();;
+    p.sync(p);
+    var s = new Splide('.quote-slider').mount();
+    s.sync(s);
+    var b = new Splide('.fb-slider').mount();
+    b.sync(b);
+    var t = new Splide('.team-slider');
+
+    t.on("ready", () => {
+       viewport = window.innerWidth;
+       t.go('+1');
+    })
+
+    onresize = () => {
+       if ((viewport >= 768) && (window.innerWidth < 768)) {
+          t.destroy({ "completely": true });
+          t.mount();
+          t.sync(t);
+
+       }
+       if ((viewport < 768) && (window.innerWidth >= 768)) {
+          t.destroy({ "completely": true });
+          t.mount();
+          t.sync(t);
+
+       }
+    }
+    t.mount();
+    t.sync(t);
+ }
